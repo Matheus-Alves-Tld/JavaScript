f5af6fdf81cf1372197acfd076e45e856546a7e1
Matheus-Alves-Tld <128704424+Matheus-Alves-Tld@users.noreply.github.com>
2023-10-05 09:23:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play('');
+function tocaSom(){
+    document.querySelector(idElementoAudio).play('');
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0; 
 
+while (contador < 9) {
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom('#som_tecla_clap'); 
+    };
+       
+    contador = contador + 1;
+}
 listaDeTeclas[1].onclick = TocaSomClap;
 listaDeTeclas[2].onclick = TocasomTim;
 listaDeTeclas[2].onclick = TocaSomPuff;
